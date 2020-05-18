2bc6101de186c228fb71db4b4649d9156c223ae0
Josh <joshgallagher24@gmail.com>
2020-05-18 05:22:34 +0100

    Configured basic config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { WorkerModule } from './worker/worker.module';
 
 @Module({
-  imports: [WorkerModule],
+  imports: [
+    WorkerModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'test'
+        ? '.env.test'
+        : '.env'
+    }),
+  ],
   providers: [],
 })
 export class AppModule { }
